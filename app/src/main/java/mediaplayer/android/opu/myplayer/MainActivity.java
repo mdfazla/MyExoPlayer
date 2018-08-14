7afb837eb78a3f0f1e5045be15f6ebea62c9226d
mdfazla <md.fazlarabbi@gmail.com>
2018-08-15 01:54:11 +0600

    Performance improvement

--- app/src/main/java/mediaplayer/android/opu/myplayer/MainActivity.java
+++ app/src/main/java/mediaplayer/android/opu/myplayer/MainActivity.java
@@ -4,19 +4,27 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.source.dash.DashChunkSource;
 import com.google.android.exoplayer2.source.dash.DashMediaSource;
 import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DataSource;
@@ -25,12 +33,13 @@ import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.upstream.TransferListener;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Player.EventListener {
 
     private static final String mpdUrl = "https://raw.githubusercontent.com/mdfazla/RepoDashMedia/master/winter_journey_dash.mpd";//"https://raw.githubusercontent.com/mdfazla/RepoDashMedia/master/Mucize_Teaser_dash.mpd";
     private final String User_Agent = "MyExoPlayer";
     private PlayerViewModel viewModel;
     private PlayerView playerView;
+    private FrameLayout progress_holder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +51,8 @@ public class MainActivity extends AppCompatActivity {
         Utils.getDeviceResolution(this);
         playerView = findViewById(R.id.player_view);
         playerView.requestFocus();
-
+        progress_holder = findViewById(R.id.progress_holder);
+        progress_holder.setVisibility(View.GONE);
 
 
     }
@@ -50,13 +60,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-
         viewModel = ViewModelProviders.of(this).get(PlayerViewModel.class);
         if (viewModel.getMyPlayer() != null) {
             SimpleExoPlayer player = viewModel.getMyPlayer();
+            player.addListener(this);
             playerView.setPlayer(player);
-            if(!player.getPlayWhenReady())
-            player.setPlayWhenReady(true);
+            if (player.getPlayWhenReady())
+                player.setPlayWhenReady(true);
             //player.setVideoTextureView(playerView);
 
 
@@ -74,6 +84,7 @@ public class MainActivity extends AppCompatActivity {
         SimpleExoPlayer player = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
 
         playerView.setPlayer(player);
+        player.addListener(this);
 
         player.setPlayWhenReady(true);
         //player.setVideoTextureView(playerView);
@@ -101,8 +112,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        if(viewModel.getMyPlayer()!=null)
-        viewModel.getMyPlayer().setPlayWhenReady(false);
+        if (!Utils.isScreenLock(this)) {
+            if (viewModel.getMyPlayer() != null)
+                viewModel.getMyPlayer().setPlayWhenReady(false);
+        }
     }
 
     @Override
@@ -117,10 +130,68 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        playerView=null;
+        if (viewModel.getMyPlayer() != null)
+            viewModel.getMyPlayer().removeListener(this);
+        playerView = null;
+        progress_holder = null;
+
     }
 
-    private void addToWindowManager(){
+    private void addToWindowManager() {
+
+    }
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest, int reason) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+        if (playbackState == Player.STATE_BUFFERING) {
+            progress_holder.setVisibility(View.VISIBLE);
+        } else if (playbackState == Player.STATE_READY) {
+            progress_holder.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onRepeatModeChanged(int repeatMode) {
+
+    }
+
+    @Override
+    public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity(int reason) {
+
+    }
+
+    @Override
+    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+    }
+
+    @Override
+    public void onSeekProcessed() {
 
     }
 }
--- app/src/main/java/mediaplayer/android/opu/myplayer/Utils.java
+++ app/src/main/java/mediaplayer/android/opu/myplayer/Utils.java
@@ -1,7 +1,11 @@
 package mediaplayer.android.opu.myplayer;
 
 import android.graphics.Point;
+import android.os.Build;
+import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
+
+import static android.content.Context.POWER_SERVICE;
 
 public class Utils {
     private static Point res;
@@ -14,5 +18,17 @@ public class Utils {
 
     public static Point getRes() {
         return res;
+    }
+
+    public static boolean isScreenLock(AppCompatActivity activity) {
+        PowerManager powerManager = (PowerManager) activity.getSystemService(POWER_SERVICE);
+        boolean isScreenOn;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH) {
+            isScreenOn = powerManager.isInteractive();
+        } else {
+            isScreenOn = powerManager.isScreenOn();
+        }
+
+        return isScreenOn;
     }
 }
